8d19d0bfabd77aa44a007ec5f188e3d23bdae038
DattaKatakam <dattakatakam21@gmail.com>
2024-05-31 14:27:54 +0530

    changes in tsconfig.json, and npm run build

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ let currentPopup: any = undefined;
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags)
-
+    
     WA.room.onEnterLayer('clockZone').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
