c21b2178daa5b4ec22bcbdcb96a6ba7b07de855d
Milorico <juancamigarcia@gmail.com>
2024-06-04 21:44:25 +0200

    Answers Index adjusted
    - last thing to add, the trophy or reward for reaching the end  !Victory!

--- src/index.ts
+++ src/index.ts
@@ -54,8 +54,8 @@ let isUnlockedDoorLayer1 = false;
 
 let popupText2 = "Wann und wo findet die Jahrestagung von eTeach-Netzwerk start?";
 let popupAnswers2 = ["04-05.12.2024 in FSU Jena", "10-11.12.2024 in HU Berlin"];
-let answer2 = "Falsch! ist am 04.-05. November 2024, in FSU Jena";
-let second_text2 = "Richtig, 04.-05. November 2024, in FSU Jena";
+let answer2 = "Richtig, 04.-05. November 2024, in FSU Jena";
+let second_text2 = "Falsch! ist am 04.-05. November 2024, in FSU Jena"; 
 // Define the initial and secondary layers
 let enablePopupLayer2 = "myLayer2";
 let doorLayer2 = "myDoor2";
@@ -67,8 +67,8 @@ let isUnlockedDoorLayer2 = false;
 
 let popupText3 = "Welche ist eines der Schwerpunktthemen des Jahres im eTeach-Netzwerk?";
 let popupAnswers3 = ["KI", " Tools, Medientechnik", "Hybride Lehre"];
-let answer3 = "Falsch! ist KI";
-let second_text3 = "Richtig, KI";
+let answer3 = "Richtig, KI";
+let second_text3 = "Falsch! ist KI"; 
 // Define the initial and secondary layers
 //let enablePopupLayer3 = "myLayer3";
 let doorLayer3 = "myDoor3";
@@ -80,8 +80,8 @@ let isUnlockedDoorLayer3 = false;
 
 let popupText4 = "Wie viele Kontaktstellen haben wir im Netzwerk der Thüringer Hochschulen und Universitäten?";
 let popupAnswers4 = ["11", " 15"];
-let answer4 = "Falsch! ist 11";
-let second_text4 = "Richtig, 11";
+let answer4 = "Richtig, 11";
+let second_text4 = "Falsch! ist 11";
 // Define the initial and secondary layers
 //let enablePopupLayer3 = "myLayer3";
 let doorLayer4 = "myDoor4";
@@ -93,8 +93,8 @@ let isUnlockedDoorLayer4 = false;
 
 let popupText5 = "Welche Veranstaltung findet einmal im Monat für die Lehrenden statt?";
 let popupAnswers5 = ["Innovation@eTeach", " Talks@eTeach"];
-let answer5 = "Falsch! ist Talks@eTeach";
-let second_text5 = "Richtig, Talks@eTeach";
+let answer5 = "Richtig, Talks@eTeach";
+let second_text5 = "Falsch! ist Talks@eTeach"; 
 // Define the initial and secondary layers
 //let enablePopupLayer3 = "myLayer3";
 let doorLayer5 = "myDoor5";
@@ -111,82 +111,72 @@ let popupRectangle5 = "popupRectangle5";
 let leaveLayerSubscription: any = undefined;
 //let currentPopup1  : any = undefined;
 
-async function openPopup(doorLayer: string, enablePopupLayer: string, colliderDoorLayer: string, popupRectangle: string, popupText: string, popupAnswers: string[], answer: string, second_text: string, subscriptionIndex: number) {
+async function openPopup(doorLayer: string, enablePopupLayer: string, colliderDoorLayer: string, popupRectangle: string, popupText: string, popupAnswers: string[], correctAnswerIndex: number, answer: string, second_text: string, subscriptionIndex: number) {
     if (currentPopup) return;
-    currentPopup = WA.ui.openPopup(popupRectangle, popupText, [{
-        label: popupAnswers[0],
+    currentPopup = WA.ui.openPopup(popupRectangle, popupText, popupAnswers.map((popupAnswer, index) => ({
+        label: popupAnswer,
         className: "primary",
         callback: () => {
             currentPopup?.close();
             currentPopup = null;
-            currentPopup = WA.ui.openPopup(popupRectangle, second_text, [{
-                label: "Schließen",
-                className: "primary",
-                callback: () => {
-                    currentPopup?.close();
-                    currentPopup = null;
-                }
-            }]);
-        }
-    },
-    {
-        label: popupAnswers[1],
-        className: "primary",
-        callback: () => {
-            currentPopup?.close();
-                    currentPopup = null;
-            currentPopup = WA.ui.openPopup(popupRectangle, answer, [{
-                label: "Tür öffnen",
-                className: "primary",
-                callback: () => {
-                    currentPopup?.close();
-                    currentPopup = null;
-                    WA.room.hideLayer(enablePopupLayer);
-                    WA.room.hideLayer(doorLayer);
-                    WA.room.hideLayer(colliderDoorLayer);
-                    // unsubscribe
-                    if (subscriptionIndex === 1) {
-                        subscription1.unsubscribe();
-                    } else if (subscriptionIndex === 2) {
-                        subscription2.unsubscribe();
-                    } else if (subscriptionIndex === 3) {
-                        subscription3.unsubscribe();
-                    } else if (subscriptionIndex === 4) {
-                        subscription4.unsubscribe();
-                    } else if (subscriptionIndex === 5) {
-                        subscription5.unsubscribe();
+            if (index === correctAnswerIndex) {
+                currentPopup = WA.ui.openPopup(popupRectangle, answer, [{
+                    label: "Tür öffnen",
+                    className: "primary",
+                    callback: () => {
+                        currentPopup?.close();
+                        currentPopup = null;
+                        WA.room.hideLayer(enablePopupLayer);
+                        WA.room.hideLayer(doorLayer);
+                        WA.room.hideLayer(colliderDoorLayer);
+                        if (subscriptionIndex === 1) {
+                            subscription1.unsubscribe();
+                        } else if (subscriptionIndex === 2) {
+                            subscription2.unsubscribe();
+                        } else if (subscriptionIndex === 3) {
+                            subscription3.unsubscribe();
+                        } else if (subscriptionIndex === 4) {
+                            subscription4.unsubscribe();
+                        } else if (subscriptionIndex === 5) {
+                            subscription5.unsubscribe();
+                        }
                     }
-                }
-            }]);
+                }]);
+            } else {
+                currentPopup = WA.ui.openPopup(popupRectangle, second_text, [{
+                    label: "Schließen",
+                    className: "primary",
+                    callback: () => {
+                        currentPopup?.close();
+                        currentPopup = null;
+                    }
+                }]);
+            }
         }
-    }
-    ]);
+    })));
 
     WA.room.onLeaveLayer(enablePopupLayer).subscribe(() => {
         currentPopup?.close();
         currentPopup = null;
     });
 }
-
 // Example usage
 const subscription1 = WA.room.onEnterLayer(enablePopupLayer1).subscribe(async () => {
-    await openPopup(doorLayer1, enablePopupLayer1, colliderDoorLayer1, popupRectangle, popupText, popupAnswers, answer, second_text, 1);
-});
+    await openPopup(doorLayer1, enablePopupLayer1, colliderDoorLayer1, popupRectangle, popupText, popupAnswers, 1, answer, second_text, 1);});
 
 const subscription2 = WA.room.onEnterLayer(enablePopupLayer2).subscribe(async () => {
-    await openPopup(doorLayer2, enablePopupLayer2, colliderDoorLayer2, popupRectangle2, popupText2, popupAnswers2, answer2, second_text2, 2);
-});
+    await openPopup(doorLayer2, enablePopupLayer2, colliderDoorLayer2, popupRectangle2, popupText2, popupAnswers2, 0, answer2, second_text2, 2);});
 
 const subscription3 = WA.room.onEnterLayer(enablePopupLayer3).subscribe(async () => {
-    await openPopup(doorLayer3, enablePopupLayer3, colliderDoorLayer3, popupRectangle3, popupText3, popupAnswers3, answer3, second_text3, 3);
+    await openPopup(doorLayer3, enablePopupLayer3, colliderDoorLayer3, popupRectangle3, popupText3, popupAnswers3, 0, answer3, second_text3, 3);
 });
 
 const subscription4 = WA.room.onEnterLayer(enablePopupLayer4).subscribe(async () => {
-    await openPopup(doorLayer4, enablePopupLayer4, colliderDoorLayer4, popupRectangle4, popupText4, popupAnswers4, answer4, second_text4, 4);
+    await openPopup(doorLayer4, enablePopupLayer4, colliderDoorLayer4, popupRectangle4, popupText4, popupAnswers4, 0, answer4, second_text4, 4);
 });
 
 const subscription5 = leaveLayerSubscription = WA.room.onEnterLayer(enablePopupLayer5).subscribe(async () => {
-    await openPopup(doorLayer5, enablePopupLayer5, colliderDoorLayer5, popupRectangle5, popupText5, popupAnswers5, answer5, second_text5, 5);
+    await openPopup(doorLayer5, enablePopupLayer5, colliderDoorLayer5, popupRectangle5, popupText5, popupAnswers5, 1, answer5, second_text5, 5);
 });
 
 
